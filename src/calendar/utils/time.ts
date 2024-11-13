136d8e4a8d09467e16428b9fd618f9c1c72d47ae
Aude Richon <auderichon@gmail.com>
2024-11-13 11:35:17 +0100

    🎨 [src/calendar] improve time utils functions

--- src/calendar/utils/time.ts
+++ src/calendar/utils/time.ts
@@ -2,19 +2,31 @@ import { EventInputInterface, TimeSlotType } from "../../types";
 
 export const getTimeStringInMinutes = (time: string): number => {
   const hoursMinutesArray = time.split(":");
+  if (hoursMinutesArray.length !== 2) {
+    return 0;
+  }
+
   const hours = Number(hoursMinutesArray[0]);
   const minutes = Number(hoursMinutesArray[1]);
-  return hours * 60 + minutes;
+  return isNaN(hours) || isNaN(minutes) ? 0 : hours * 60 + minutes;
 };
 
+// NB: this function only works for same day start and end times
 export const getDurationInMinutes = ({
   startTime,
   endTime,
+  allowNegativeDuration = false,
 }: {
   startTime: string;
   endTime: string;
-}) => getTimeStringInMinutes(endTime) - getTimeStringInMinutes(startTime);
+  allowNegativeDuration?: boolean;
+}) => {
+  const duration =
+    getTimeStringInMinutes(endTime) - getTimeStringInMinutes(startTime);
+  return duration >= 0 || allowNegativeDuration ? duration : 0;
+};
 
+// NB: this function only works for same day event and start time
 export const getEventTimeSlot = (
   event: EventInputInterface,
   startTime: string
@@ -22,6 +34,7 @@ export const getEventTimeSlot = (
   const startTimePosition = getDurationInMinutes({
     startTime,
     endTime: event.start,
+    allowNegativeDuration: true,
   });
   return [startTimePosition, startTimePosition + event.duration];
 };
